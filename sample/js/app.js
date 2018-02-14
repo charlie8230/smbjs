0048c18e730c51ba54998901b0eaa470027bcc9b
carlos <carlos@moranjr.com>
2018-02-14 12:40:29 -0500

    work

--- sample/js/app.js
+++ sample/js/app.js
@@ -3,5 +3,16 @@
 
 */
 console.log(typeof T3);
-SMBJS.app.addModule('gigya',(context)=>{console.log(context); return{init(){}}});
+SMBJS.app.addModule('gigya',(context)=>{console.log(context); return{behaviors:['logger'],init(){}}});
+SMBJS.app.addBehavior('logger',(context)=>{ return {init(){
+  console.log('I\'m a behavior!', context);
+  let svc = context.getService('string');
+  console.log(svc);
+  svc.run();
+}}});
+SMBJS.app.addService('string',(application)=>{
+  return { run(){
+    console.log('I\'m a service!', application);
+  }}
+});
 SMBJS.app.init();
